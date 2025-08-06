3b96032c642b76c449ff2171d5415a860e20b1d8
Aasthayuli <aasthayuli2000@gmail.com>
2025-08-07 00:32:39 +0530

    added upto day60

---
+++ upgraded-blog/main.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request
+import requests
+import smtplib
+import requests
+
+posts = requests.get("https://api.npoint.io/d1ea67506a14fcfeacaf").json()
+OWN_EMAIL = "YOUR OWN EMAIL ADDRESS"
+OWN_PASSWORD = "YOUR EMAIL ADDRESS PASSWORD"
+app = Flask(__name__)
+
+@app. route('/')
+def home():
+    return render_template("index.html", all_posts=posts)
+
+
+@app. route("/about")
+def about():
+    return render_template("about.html")
+
+# SOLUTION to Challenge:
+@app.route("/form-entry", methods=["POST"])
+def receive_data():
+    data = request.form
+    print(data["name"])
+    print(data["email"])
+    print(data["phone"])
+    print(data["message"])
+    return render_template("success.html")
+
+
+
+url = "https://api.npoint.io/d1ea67506a14fcfeacaf"
+response = requests.get(url)
+posts = response.json()
+
+@app.route("/post/<int:index>")
+def post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
+
+if __name__ == "__main__":
+    app.run(debug=True)
